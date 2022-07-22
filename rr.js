9f79cdca32afe21fb9dee9906b2cb45c23af613f
Advaith.V.A <97515865+Advaithva@users.noreply.github.com>
2022-07-22 17:05:17 +0530

    Update rr.js

--- rr.js
+++ rr.js
@@ -1,16 +1,9 @@
 function RICKROLL() {
-	alert("We're no strangers to love
-You know the rules, and so do I
-A full commitment is what I'm thinking of
-You wouldn't get this from any other guy
 
-I just wanna tell you how I'm feeling
-Gotta make you, understand
-
-Never gonna give you up
-Never gonna let you down
-Never gonna run around and desert you
-Never gonna make you cry
-Never gonna say goodbye
-Never gonna tell a lie, and hurt you");
+alert("Never gonna give you up")
+alert("Never gonna let you down")
+alert("Never gonna run around and desert you")
+alert"Never gonna make you cry")
+alert("Never gonna say goodbye")
+alert("Never gonna tell a lie, and hurt you");
 }
