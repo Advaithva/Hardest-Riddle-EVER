174b83ff90eb6b0b3c8ac0273b1b7d73bf55424a
Advaith.V.A <97515865+Advaithva@users.noreply.github.com>
2022-07-23 22:02:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,4 +6,16 @@ alert("Never gonna run around and desert you");
 alert("Never gonna make you cry");
 alert("Never gonna say goodbye");
 alert("Never gonna tell a lie, and hurt you");
+  alert("Never gonna give you up");
+alert("Never gonna let you down");
+alert("Never gonna run around and desert you");
+alert("Never gonna make you cry");
+alert("Never gonna say goodbye");
+alert("Never gonna tell a lie, and hurt you");
+  alert("Never gonna give you up");
+alert("Never gonna let you down");
+alert("Never gonna run around and desert you");
+alert("Never gonna make you cry");
+alert("Never gonna say goodbye");
+alert("Never gonna tell a lie, and hurt you");
 }
