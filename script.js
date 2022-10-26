6df88694534cf02fa4144b12e1f2aa0b129cbcaa
Advaith.V.A <97515865+Advaithva@users.noreply.github.com>
2022-10-26 14:48:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,4 +18,115 @@ alert("Never gonna run around and desert you");
 alert("Never gonna make you cry");
 alert("Never gonna say goodbye");
 alert("Never gonna tell a lie, and hurt you");
+  
+}
+function PASS() {
+
+alert("Never gonna give you up");
+alert("Never gonna let you down");
+alert("Never gonna run around and desert you");
+alert("Never gonna make you cry");
+alert("Never gonna say goodbye");
+alert("Never gonna tell a lie, and hurt you");
+  alert("Never gonna give you up");
+alert("Never gonna let you down");
+alert("Never gonna run around and desert you");
+alert("Never gonna make you cry ");
+alert("Never gonna say goodbye");
+alert("Never gonna tell a lie, and hurt you");
+  alert("Never gonna give you up");
+alert("Never gonna let you down");
+alert("Never gonna run around and desert you");
+alert("Never gonna make you cry");
+alert("Never gonna say goodbye");
+alert("Never gonna tell a lie, and hurt you");
+  
+}
+function PASS() {
+
+alert("Never gonna give you up");
+alert("Never gonna let you down");
+alert("Never gonna run around and desert you");
+alert("Never gonna make you cry");
+alert("Never gonna say goodbye");
+alert("Never gonna tell a lie, and hurt you");
+  alert("Never gonna give you up");
+alert("Never gonna let you down");
+alert("Never gonna run around and desert you");
+alert("Never gonna make you cry ");
+alert("Never gonna say goodbye");
+alert("Never gonna tell a lie, and hurt you");
+  alert("Never gonna give you up");
+alert("Never gonna let you down");
+alert("Never gonna run around and desert you");
+alert("Never gonna make you cry");
+alert("Never gonna say goodbye");
+alert("Never gonna tell a lie, and hurt you");
+  
+}
+function PASS() {
+
+alert("Never gonna give you up");
+alert("Never gonna let you down");
+alert("Never gonna run around and desert you");
+alert("Never gonna make you cry");
+alert("Never gonna say goodbye");
+alert("Never gonna tell a lie, and hurt you");
+  alert("Never gonna give you up");
+alert("Never gonna let you down");
+alert("Never gonna run around and desert you");
+alert("Never gonna make you cry ");
+alert("Never gonna say goodbye");
+alert("Never gonna tell a lie, and hurt you");
+  alert("Never gonna give you up");
+alert("Never gonna let you down");
+alert("Never gonna run around and desert you");
+alert("Never gonna make you cry");
+alert("Never gonna say goodbye");
+alert("Never gonna tell a lie, and hurt you");
+  
+}
+function PASS() {
+
+alert("Never gonna give you up");
+alert("Never gonna let you down");
+alert("Never gonna run around and desert you");
+alert("Never gonna make you cry");
+alert("Never gonna say goodbye");
+alert("Never gonna tell a lie, and hurt you");
+  alert("Never gonna give you up");
+alert("Never gonna let you down");
+alert("Never gonna run around and desert you");
+alert("Never gonna make you cry ");
+alert("Never gonna say goodbye");
+alert("Never gonna tell a lie, and hurt you");
+  alert("Never gonna give you up");
+alert("Never gonna let you down");
+alert("Never gonna run around and desert you");
+alert("Never gonna make you cry");
+alert("Never gonna say goodbye");
+alert("Never gonna tell a lie, and hurt you");
+  
+}
+function PASS() {
+
+alert("Never gonna give you up");
+alert("Never gonna let you down");
+alert("Never gonna run around and desert you");
+alert("Never gonna make you cry");
+alert("Never gonna say goodbye");
+alert("Never gonna tell a lie, and hurt you");
+  alert("Never gonna give you up");
+alert("Never gonna let you down");
+alert("Never gonna run around and desert you");
+alert("Never gonna make you cry ");
+alert("Never gonna say goodbye");
+alert("Never gonna tell a lie, and hurt you");
+  alert("Never gonna give you up");
+alert("Never gonna let you down");
+alert("Never gonna run around and desert you");
+alert("Never gonna make you cry");
+alert("Never gonna say goodbye");
+alert("Never gonna tell a lie, and hurt you");
+  
 }
